12578d7446e767bff331fa017e30845e5100a7e8
ReCreate <info.recreate.data@gmail.com>
2025-01-28 18:30:28 -0400

    agregado de funcionalidad y nuevas secciones al portafolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,13 +34,35 @@ tabs.forEach(tab => {
 })
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
-
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+})
 /*===== Link Active Work =====*/
 
+const linkWork = document.querySelectorAll('.work__item')
 
+function activeWork(){
+    linkWork.forEach(l => l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+linkWork.forEach(l => l.addEventListener("click", activeWork))
 /*===== Work Popup =====*/
 
+document.addEventListener("click", (e) => {
+    if(e.target.classList.contains("work__button")){
+        togglePortfolioPopup();
+    }
+})
+function togglePortfolioPopup() {
+    document.querySelector(".portfolio__popup").classList.toggle("open");
+}
 
+ document.querySelector(".portfolio__popup-close").addEventListener("click", togglePortfolioPopup)
 /*=============== SERVICES MODAL ===============*/
 
 
